3e441d56505952c7113d60acb8423b474cecf9b8
Amey Jadiye <ameyjadiye@gmail.com>
2019-06-05 16:02:57 +0530

    Project Euler : Problem 1

---
+++ projecteuler/src/main/java/com/codeinventory/projecteuler/P1.java
@@ -0,0 +1,21 @@
+package com.codeinventory.projecteuler;
+
+public class P1 {
+
+    public static void main(String ... args) {
+
+        int sum = 0;
+
+        for (int i = 0;i < 1000;i++) {
+
+            if (i % 3 == 0) {
+                sum += i;
+            } else if (i % 5 == 0) {
+                sum += i;
+            }
+
+        }
+
+        System.out.println(sum);
+    }
+}
